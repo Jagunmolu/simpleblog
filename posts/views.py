abb28abee0e4120bc115212ddbe3190c8b00edcc
Jagunmolu <kolawolejonathan@gmail.com>
2022-06-29 20:24:41 +0100

    added a simple post logic in the views.py

--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=['GET', 'POST'])
 def homepage(request:Request):
+
+    if request.method == 'POST':
+        data = request.data
+        response = {'message': 'Post created successfully', 'data': data}
+        return Response(data=response, status=status.HTTP_201_CREATED)
     response = {'message': 'This is a function-based view and it is only temporary.'}
     return Response(data=response, status=status.HTTP_200_OK)
