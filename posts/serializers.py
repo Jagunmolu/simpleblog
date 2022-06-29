5843523c68b459aaaa7f56baed7be2b8a87c2877
Jagunmolu <kolawolejonathan@gmail.com>
2022-06-29 21:43:05 +0100

    changed the serializers.py file to adopt the serializers.ModelSerializer format.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,11 @@
+from dataclasses import field
+import imp
 from turtle import title
 from rest_framework import serializers
+from .models import Post
 
 
 class PostSerializer(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    title=serializers.CharField(max_length=50)
-    content=serializers.CharField()
-    created_at=serializers.DateTimeField(read_only=True)
-    
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'created_at']
