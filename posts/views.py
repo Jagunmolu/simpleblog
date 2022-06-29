df1af48256554911c84409467bed41fe58c7fef1
Jagunmolu <kolawolejonathan@gmail.com>
2022-06-29 19:33:11 +0100

    added a temporary function-based view toviews.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import imp
+import re
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+def homepage(request:Request):
+    response = {'message': 'This is a function-based view and it is only temporary.'}
+    return Response(data=response, status=status.HTTP_200_OK)
