b6f67d7c1f9d3cd470911492514122fbd3a64d98
Jagunmolu <kolawolejonathan@gmail.com>
2022-06-29 19:18:15 +0100

    added posts and rest_framework to settings.py and changed timezone to Africa/Lagos

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+
+    #third-party-apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Lagos'
 
 USE_I18N = True
 
