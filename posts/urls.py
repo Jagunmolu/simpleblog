759e9b61d3df1ac1ce88d98355d75a001ffa0da4
Jagunmolu <kolawolejonathan@gmail.com>
2022-06-29 23:19:26 +0100

    added two new function-based views

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.homepage, name='home')
+    path('home/', views.homepage, name='home'),
+    path('', views.list_posts, name='all_posts'),
+    path('<int:id>/', views.list_post_by_id, name='single_posts'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,15 @@
 import imp
 import re
+from tkinter.messagebox import RETRY
+from urllib import response
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from .models import Post
+from .serializers import PostSerializer
+from posts import serializers
+
 
 @api_view(http_method_names=['GET', 'POST'])
 def homepage(request:Request):
@@ -14,3 +20,29 @@ def homepage(request:Request):
         return Response(data=response, status=status.HTTP_201_CREATED)
     response = {'message': 'This is a function-based view and it is only temporary.'}
     return Response(data=response, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=['GET'])
+def list_posts(request:Request):
+    posts = Post.objects.all()
+
+    serializer = PostSerializer(instance=posts, many=True)
+
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=['GET'])
+def list_post_by_id(request:Request, id:int):
+    post = Post.objects.get(id=id)
+
+    serializer = PostSerializer(instance=post)
+
+    if not serializer:
+        return Response(serializer.error)
+
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+# @api_view(http_method_names=['PUT'])
+# def edit_post(request:Request, id:int):
+
+#     data = request.data
+#     response = {'message': 'Post created successfully', 'data': data}
+#     return Response(data=response, status=status.HTTP_201_CREATED)
