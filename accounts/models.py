6dbbcbb7681fdcd5dda697320596ada265c6dccb
Jagunmolu <kolawolejonathan@gmail.com>
2022-07-01 07:07:10 +0100

    implemented authentications by creating accounts app.

---
+++ accounts/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+
+# from django.contrib.auth.models import AbstractBaseUser
+# from django.contrib.auth.base_user import AbstractBaseUser
+# What is/are the differences between the above 2?
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.setpassword(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True),
+        extra_fields.setdefault('is_superuser', True)
+        if not extra_fields.get('is_staff'):
+            raise ValueError('superuser must have is_staff to be true.')
+
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('superuser must have is_superuser to be true.')
+
+        return self.create_user(email=email, password=password, **extra_fields)
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=25)
+    date_of_birth = models.DateField(null=True)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
