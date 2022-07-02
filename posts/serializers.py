13d76f98d83f92b695ca5ccdb6f783a1a7f673e9
Jagunmolu <kolawolejonathan@gmail.com>
2022-07-02 15:38:47 +0100

    sorted imports

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,5 @@
-from dataclasses import field
-import imp
-from turtle import title
 from rest_framework import serializers
+
 from .models import Post
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+
 from .models import Post
 from .serializers import PostSerializer
-
 
 
 class PostViewset(viewsets.ModelViewSet):
