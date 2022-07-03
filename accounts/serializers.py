d25e4968cace625311734084bc31eeac8797938e
Jagunmolu <kolawolejonathan@gmail.com>
2022-07-03 08:28:59 +0100

    implemented a signup setup

---
+++ accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from rest_framework.validators import ValidationError
+from .models import User
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField()
+    username = serializers.CharField(max_length=15)
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+
+        model = User
+        fields = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+
+        email_exists = User.objects.filter(email=attrs['email']).exists
+        username_exists = User.objects.filter(username=attrs['username']).exists
+
+        if email_exists and username_exists:
+            raise ValidationError('Email and Username have been used')
+        if email_exists:
+            raise ValidationError('Email has been used')
+        if username_exists:
+            raise ValidationError('Username has been used')
+        return super().validate(attrs)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import SignUpSerializer
+
+
+class SignUpView(generics.GenericAPIView):
+
+    serializer_class = SignUpSerializer
+
+    def post(self, request:Request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+
+        response = {
+            'message': 'User has been created successfully',
+            'data': serializer.data
+        }
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
