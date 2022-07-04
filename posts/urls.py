8e07091838a5b03f1c0c19cc47fcaaf342ffdb7d
Jagunmolu <kolawolejonathan@gmail.com>
2022-07-04 19:48:19 +0100

    changed views to ListCreateAPIView and RetrieveUpdateDestroyAPIView.

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path('', views.PostListCreateView.as_view(), name='all_posts'),
-#     path('<int:pk>/', views.PostRetrieveUpdateDeleteView.as_view(), name='single_posts'),
-# ]
+urlpatterns = [
+    path('', views.PostListCreateView.as_view(), name='all_posts'),
+    path('<int:pk>/', views.PostRetrieveUpdateDeleteView.as_view(), name='single_posts'),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, mixins, generics
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -8,7 +8,36 @@ from .models import Post
 from .serializers import PostSerializer
 
 
-class PostViewset(viewsets.ModelViewSet):
-    authentication_classes = [IsAuthenticated]
-    queryset = Post.objects.all()
+# class PostViewset(viewsets.ModelViewSet):
+#     authentication_classes = [IsAuthenticated]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+class PostListCreateView(generics.ListCreateAPIView):
+
     serializer_class = PostSerializer
+    permission_classes = []
+    queryset = Post.objects.all()
+
+    # def get(self, request:Request, *args, **kwargs):
+    #     return self.list(request, *args, **kwargs)
+
+        
+    # def post(self, request:Request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
+
+
+class PostRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = PostSerializer
+    permission_classes = []
+    queryset = Post.objects.all()
+    
+    # def put(self, request:Request, *args, **kwargs):
+    #     pass
+
+    # def get(self, request:Request, *args, **kwargs):
+    #     pass
+
+    # def destroy(self, request, *args, **kwargs):
+    #     return super().destroy(request, *args, **kwargs)
